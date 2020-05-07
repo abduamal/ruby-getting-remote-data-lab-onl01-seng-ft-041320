bedf1f1df34bd54cd39752d5a9d6be506216efad
Amal Abdu <abduamal@ymail.com>
2020-05-07 12:42:51 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,11 +4,19 @@ require 'open-uri'
 
 
 class GetRequester
+
   URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
+
   def initialize
 
   end
+
+  def get_response_body
+
+  end
+
   def parse_json
 
   end
+
 end
