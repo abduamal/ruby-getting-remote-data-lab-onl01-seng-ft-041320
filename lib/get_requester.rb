f33edd5faeee3b44dcd77801b643165b90d77254
Amal Abdu <abduamal@ymail.com>
2020-05-07 12:59:14 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,8 +12,8 @@ class GetRequester
 
   def get_response_body
     uri = URI.parse(@url)
-    response = NET::HTTP.get_response(uri)
-    response.body
+    # response = NET::HTTP.get_response(uri)
+    # response.body
   end
 
   def parse_json
